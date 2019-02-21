37c1763ced874bfb4a5a700379504e89868aeaa8
Haoyu ZUO <zhyphil@gmail.com>
2019-02-21 11:16:17 +0100

    Exercise 2

--- server/server.js
+++ server/server.js
@@ -45,5 +45,16 @@ app.put('/lions/:id', function(req, res) {
   }
 });
 
+app.delete('/lions/:id', function(req,res){
+  var lion = _.findIndex(lions, {id: req.params.id});
+  if(!lions[lion]){
+    res.send();
+  }else{
+    var deletedLion = lions[lion];
+    lions.splice(lion,1);
+    res.json(deletedLion);
+  }
+});
+
 app.listen(3000);
 console.log('on port 3000');
