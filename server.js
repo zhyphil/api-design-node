ab790db96f661c31a3f29d05e38eb80a1176b1c7
Haoyu ZUO <zhyphil@gmail.com>
2019-02-21 10:52:34 +0100

    Exercise 1

--- server.js
+++ server.js
@@ -2,15 +2,21 @@ var express = require('express');
 var app = express();
 
 var jsonData = {count: 12, message: 'hey'};
+var fs = require('fs');
 
 app.get('/', function(req, res){
   // res.sendFile takes an absolute path to a file and
   // sets the mime type based n the file extname
-  res.sendFile(__dirname + '/index.html', function(err) {
-    if (err) {
-      res.status(500).send(err);
-    }
-  })
+  // res.sendFile(__dirname + '/index.html', function(err) {
+  //   if (err) {
+  //     res.status(500).send(err);
+  //   }
+  // })
+  fs.readFile('index.html', function(err, buffer){
+    var html = buffer.toString();
+    res.setHeader('Content-Type', 'text/html');
+    res.send(html);
+  });
 });
 
 app.get('/data', function(req, res) {
